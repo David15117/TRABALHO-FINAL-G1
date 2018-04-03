a41224b86be59f932285bce7dc45f326f10124c8
david xavier <David15117>
2018-04-02 22:07:27 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,24 @@ import {Disciplina} from './disciplina.model';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+    constructor() {
+        this.atualizarLocalStorage();
+    }
+
     info: string = null;
     selecionado = null;
-    exibir= false;
+    exibir = false;
+    data: Date= null;
+    ativa = null;
+    tipo = null;
     codigo: number = null;
     nome: string = null;
-    professor:string = null;
+    periodo=null;
+    professor: string = null;
     descricao: string = '';
     disciplinas = [
-        new Disciplina(2525,'Lingua Portuguesa','David',
+        new Disciplina(2525, 'Lingua Portuguesa', 'David',
             'O objetivo norteador da BNCC de Língua Portuguesa é ' +
             'garantir a todos os alunos o acesso aos saberes ' +
             'linguísticos necessários para a participação social e o exercício ' +
@@ -22,7 +31,7 @@ export class AppComponent {
             'o ser humano pensa, comunica-se, tem acesso à informação, expressa ' +
             'e defende pontos de vista, partilha ou ' +
             'constrói visões de mundo e produz conhecimento.'),
-        new Disciplina(25252,'Arte', 'Marcus','2O objetivo norteador da BNCC de Língua Portuguesa é ' +
+        new Disciplina(25252, 'Arte', 'Marcus', '2O objetivo norteador da BNCC de Língua Portuguesa é ' +
             'garantir a todos os alunos o acesso aos saberes ' +
             'linguísticos necessários para a participação social e o exercício ' +
             'da cidadania, pois é por meio da língua que ' +
@@ -30,61 +39,61 @@ export class AppComponent {
             'e defende pontos de vista, partilha ou ' +
             'constrói visões de mundo e produz conhecimento.'
         ),
-        new Disciplina(263985,'Edutação Fisica', 'Xavier','3O objetivo norteador da BNCC de Língua Portuguesa é ' +
+        new Disciplina(263985, 'Edutação Fisica', 'Xavier', '3O objetivo norteador da BNCC de Língua Portuguesa é ' +
             'garantir a todos os alunos o acesso aos saberes ' +
             'linguísticos necessários para a participação social e o exercício ' +
             'da cidadania, pois é por meio da língua que ' +
             'o ser humano pensa, comunica-se, tem acesso à informação, expressa ' +
             'e defende pontos de vista, partilha ou ' +
             'constrói visões de mundo e produz conhecimento.'),
-        new Disciplina(2563978,'Matematica', 'João','4O objetivo norteador da BNCC de Língua Portuguesa é ' +
+        new Disciplina(2563978, 'Matematica', 'João', '4O objetivo norteador da BNCC de Língua Portuguesa é ' +
             'garantir a todos os alunos o acesso aos saberes ' +
             'linguísticos necessários para a participação social e o exercício ' +
             'da cidadania, pois é por meio da língua que ' +
             'o ser humano pensa, comunica-se, tem acesso à informação, expressa ' +
             'e defende pontos de vista, partilha ou ' +
             'constrói visões de mundo e produz conhecimento.'),
-        new Disciplina(22525,'Historia',
-            'Pedro','656 objetivo norteador da BNCC de Língua Portuguesa é ' +
+        new Disciplina(22525, 'Historia',
+            'Pedro', '656 objetivo norteador da BNCC de Língua Portuguesa é ' +
             'garantir a todos os alunos o acesso aos saberes ' +
             'linguísticos necessários para a participação social e o exercício ' +
             'da cidadania, pois é por meio da língua que ' +
             'o ser humano pensa, comunica-se, tem acesso à informação, expressa ' +
             'e defende pontos de vista, partilha ou ' +
             'constrói visões de mundo e produz conhecimento.'),
-        new Disciplina(69969,'Geografia',
-            'Madia','3432O objetivo norteador da BNCC de Língua Portuguesa é ' +
+        new Disciplina(69969, 'Geografia',
+            'Madia', '3432O objetivo norteador da BNCC de Língua Portuguesa é ' +
             'garantir a todos os alunos o acesso aos saberes ' +
             'linguísticos necessários para a participação social e o exercício ' +
             'da cidadania, pois é por meio da língua que ' +
             'o ser humano pensa, comunica-se, tem acesso à informação, expressa ' +
             'e defende pontos de vista, partilha ou ' +
             'constrói visões de mundo e produz conhecimento.'),
-        new Disciplina(6989595,'Ciencias', 'Fabiano','23432O objetivo norteador da BNCC de Língua Portuguesa é ' +
+        new Disciplina(6989595, 'Ciencias', 'Fabiano', '23432O objetivo norteador da BNCC de Língua Portuguesa é ' +
             'garantir a todos os alunos o acesso aos saberes ' +
             'linguísticos necessários para a participação social e o exercício ' +
             'da cidadania, pois é por meio da língua que ' +
             'o ser humano pensa, comunica-se, tem acesso à informação, expressa ' +
             'e defende pontos de vista, partilha ou ' +
             'constrói visões de mundo e produz conhecimento.'),
-        new Disciplina(87987,'Redação',
-            'Fernando','32432O objetivo norteador da BNCC de Língua Portuguesa é ' +
+        new Disciplina(87987, 'Redação',
+            'Fernando', '32432O objetivo norteador da BNCC de Língua Portuguesa é ' +
             'garantir a todos os alunos o acesso aos saberes ' +
             'linguísticos necessários para a participação social e o exercício ' +
             'da cidadania, pois é por meio da língua que ' +
             'o ser humano pensa, comunica-se, tem acesso à informação, expressa ' +
             'e defende pontos de vista, partilha ou ' +
             'constrói visões de mundo e produz conhecimento.'),
-        new Disciplina(7427423,'Lingua Estrageira Moderna- Inglês',
-            'Edina',' 32432O objetivo norteador da BNCC de Língua Portuguesa é ' +
+        new Disciplina(7427423, 'Lingua Estrageira Moderna- Inglês',
+            'Edina', ' 32432O objetivo norteador da BNCC de Língua Portuguesa é ' +
             'garantir a todos os alunos o acesso aos saberes ' +
             'linguísticos necessários para a participação social e o exercício ' +
             'da cidadania, pois é por meio da língua que ' +
             'o ser humano pensa, comunica-se, tem acesso à informação, expressa ' +
             'e defende pontos de vista, partilha ou ' +
             'constrói visões de mundo e produz conhecimento.'),
-        new Disciplina(98585,'Ensino Religioso',
-            'Marla','3432O objetivo norteador da BNCC de Língua Portuguesa é ' +
+        new Disciplina(98585, 'Ensino Religioso',
+            'Marla', '3432O objetivo norteador da BNCC de Língua Portuguesa é ' +
             'garantir a todos os alunos o acesso aos saberes ' +
             'linguísticos necessários para a participação social e o exercício ' +
             'da cidadania, pois é por meio da língua que ' +
@@ -100,38 +109,68 @@ export class AppComponent {
     }
 
     salvar() {
-        const d = new Disciplina(this.codigo,this.nome,this.professor, this.descricao);
+        const d = new Disciplina(this.codigo, this.nome, this.professor, this.descricao,this.data, this.ativa,this.tipo,this.periodo);
         //console.log();
         this.disciplinas.push(d);
-        this.codigo= null;
+        localStorage.setItem("Disciplina", JSON.stringify(d));
+        this.codigo = null;
         this.nome = null;
         this.professor = null;
         this.descricao = null;
+        this.data = null;
+        this.ativa = null;
+        this.tipo = null
+        this.periodo = null;
         this.oculta('oculta');
+
     }
-  excluir(disciplina) {
-      if (confirm('Tem certeza que deseja excluir a disciplina "'
-              + disciplina.nome + '"?')) {
-          const i = this.disciplinas.indexOf(disciplina);
-          this.disciplinas.splice(i, 1);
-      }
-  }
+
+    excluir(disciplina) {
+        if (confirm('Tem certeza que deseja excluir a disciplina "'
+                + disciplina.nome + '"?')) {
+            const i = this.disciplinas.indexOf(disciplina);
+            this.disciplinas.splice(i, 1);
+            this.cancel();
+            this.oculta('exibir');
+
+        }
+    }
+
     editar(disciplina) {
         this.oculta('exibir');
         this.nome = disciplina.nome;
         this.descricao = disciplina.descricao;
         this.codigo = disciplina.codigo;
         this.professor = disciplina.professor;
+        this.data= disciplina.data;
+        this.ativa = disciplina.ativa ? 'A' : null;
+        this.tipo = disciplina.tipo;
+        this.periodo = disciplina.periodo;
 
     }
+
     cancel() {
         this.nome = null;
         this.descricao = null;
         this.codigo = null;
         this.professor = null;
         this.selecionado = null;
+        this.data = null;
+        this.ativa = null;
+        this.periodo = null
+
     }
-   oculta(local:  any) {
-        this.exibir = local ==='exibir' ? true : false;
+
+    oculta(local: any) {
+        this.exibir = local === 'exibir' ? true : false;
     }
+
+    atualizarLocalStorage() {
+        if (localStorage.getItem("Disciplina")) {
+            const z = JSON.parse(localStorage.getItem("Disciplina"));
+            this.disciplinas.push(z);
+
+        }
+    }
+
 }
--- src/app/disciplina.model.ts
+++ src/app/disciplina.model.ts
@@ -1,12 +1,22 @@
+import {isBoolean} from 'util';
+
 export class Disciplina {
     codigo: number;
     nome: string;
     professor:string;
     descricao: string;
-    constructor(codigo: number,nome: string,professor: string, descricao: string) {
+    data: Date;
+    ativa: Boolean;
+    tipo: string;
+    periodo: string;
+    constructor(codigo: number,nome: string,professor: string, descricao: string, data?: Date, ativa?:boolean,tipo?: string,periodo? : string) {
       this.codigo = codigo;
       this.nome = nome;
       this.professor = professor;
       this.descricao = descricao;
+      this.data = data;
+      this.ativa = ativa;
+      this.tipo = tipo;
+      this.periodo = periodo;
     }
   }
